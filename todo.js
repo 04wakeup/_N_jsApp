ba8c5a3f4959b82837e26b14e17a4f7b184d3398
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-11-29 18:07:06 -0800

    adding Todo List

--- todo.js
+++ todo.js
@@ -1,1 +1,38 @@
-const form = document.querySelector(".js-toDoForm");
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+
+  const delBtn = document.createElement("button"); // create & set it
+  delBtn.innerText = "❌";
+
+  const span = document.createElement("span"); // create & set it
+  span.innerText = text;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue); // Dissplay first then Save
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const toDos = localStorage.getItem(TODOS_LS);
+  if (toDos !== null) {
+  }
+}
+
+function inti() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+
+inti();
