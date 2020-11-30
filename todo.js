e5c4010599890a29a5493eea6ba953cc0e678de6
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-11-29 19:37:30 -0800

    add Delete To Do List

--- todo.js
+++ todo.js
@@ -3,7 +3,19 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
+let toDos = [];
+
+function deleteToDo(event) {
+  const btn = event.target;
+  const li = btn.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDocs = toDos.filter(function (toDo) {
+    return toDo.id != parseInt(li.id);
+  });
+  console.log(cleanToDocs);
+  toDos = cleanToDocs;
+  saveToDos(); // if ntt saved, it shows always -1 lists.
+}
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // to String
@@ -16,6 +28,7 @@ function paintToDo(text) {
   const newId = toDos.length + 1;
 
   delBtn.innerText = "❌";
+  delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
   li.appendChild(span);
