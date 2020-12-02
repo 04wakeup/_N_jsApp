5bd0fbfec871fe5ccdc3506d896d4715e573ea94
04wakeup <42279557+04wakeup@users.noreply.github.com>
2020-12-01 20:45:22 -0800

    add background

--- todo.js
+++ todo.js
@@ -8,11 +8,10 @@ let toDos = [];
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
-  toDoList.removeChild(li);
+  toDoList.removeChild(li); // remove from screen not LS
   const cleanToDocs = toDos.filter(function (toDo) {
     return toDo.id != parseInt(li.id);
   });
-  console.log(cleanToDocs);
   toDos = cleanToDocs;
   saveToDos(); // if ntt saved, it shows always -1 lists.
 }
